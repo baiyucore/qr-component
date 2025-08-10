75ee024119ae35dcfacebbc2d23fc026a0dcc765
hyt <1640260107@qq.com>
2025-08-10 16:42:08 +0800

    提交

---
+++ src/view/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card">
+    <img src="../img/image-qr-code.png" alt="qr-code" />
+    <div class="card-content">
+      <h1>Improve your front-end skills by building projects</h1>
+      <p>  Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+  
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+
+.card {
+  width: 320px;
+  height: 500px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+  padding: 12px;
+  text-align: center;
+  margin: 0 auto;
+}
+.card img {
+  width: 296px;
+  border-radius: 10px;
+}
+.card-content {
+  margin: 12px;
+}
+.card-content h1 {
+  font-size: 22px;
+  font-weight: 700;
+  color: #1f314f;
+}
+.card-content p {
+  font-size: 16px;
+  font-weight: 400;
+  color: #7d889e;
+}
+
+
+</style>
+
+
+
+<script setup lang="ts"></script>
+
