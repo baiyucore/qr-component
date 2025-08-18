6481c974dec8953b88e04661185bdaf4c9852852
hyt <1640260107@qq.com>
2025-08-18 20:08:51 +0800

    test

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   component: () => import('../view/Home.vue'),
-    // },
+    {
+      path: '/',
+      component: () => import('../view/Home.vue'),
+    },
     {
       path: '/blog_preview_card',
       component: () => import('../view/blog_preview_card.vue'),
